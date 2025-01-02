e630cf0efaad235fc69ae2e8e7c11399ffe6e755
wettestsock <evilsharkeater@gmail.com>
2025-01-02 17:40:22 -0500

    nothing of note

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,18 @@
 #include <Arduino.h>
 
+#define RED 2
+#define GREEN 4
+#define BLUE 16
 
 void setup() {
     Serial.begin(115200);
+    pinMode(RED,OUTPUT);
+    pinMode(GREEN,OUTPUT);
+    pinMode(BLUE,OUTPUT);
+
+    analogWrite(RED, 255);
+    analogWrite(BLUE, 220);
+    analogWrite(RED,104);
 }
 
 void loop() {
