a34a09a6b706fef52687750c5c2e78f5e5f032da
wettestsock <evilsharkeater@gmail.com>
2025-01-02 18:16:12 -0500

    nothing of note

--- src/main.cpp
+++ src/main.cpp
@@ -22,10 +22,6 @@ void loop() {
     analogWrite(GREEN,g);
     analogWrite(BLUE, b);
 
-    if(r==MAX && g==0 && b==0){
-    vTaskDelay(15/portTICK_PERIOD_MS);
-    
-    }
     if(r == MAX){
         if(b){
             b--;
